bc9e7f0e0bb443f1296a9b7bf2c2085a57df02bb
yuya-g <yuyaiso@icloud.com>
2024-11-20 13:48:44 +0900

    Add files via upload

---
+++ OTcomponents.py
@@ -0,0 +1,38 @@
+# ライブラリのインポート
+from prophet import Prophet
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# CSVファイルの読み込み
+data = pd.read_csv("/Users/isogaiyuya/Downloads/assignment-main/AI-Engineer/multivariate-time-series-prediction/ett.csv") 
+
+# 1. データフレーム準備
+data = data[['date', 'OT']]  # 日付と予測対象の値のみを選択
+data.rename(columns={'date': 'ds', 'OT': 'y'}, inplace=True)
+data['ds'] = pd.to_datetime(data['ds'])  # "date"列をdatetime型に変換
+data = data.dropna()  # 欠損値を削除
+
+# 2. Prophetモデルの設定
+model = Prophet()
+model.add_seasonality(name='monthly', period=30.5, fourier_order=5)  # 月次（約30.5日周期）
+model.add_seasonality(name='yearly', period=365.25, fourier_order=10)  # 年次（約365日周期）
+model.fit(data)
+
+# 5. 予測
+future = model.make_future_dataframe(periods=48, freq='H')  # 将来48時間を予測
+forecast = model.predict(future)
+
+# 6. 結果のプロット
+model.plot(forecast)
+plt.show()
+
+model.plot_components(forecast)
+plt.show()
+
+from statsmodels.graphics.tsaplots import plot_acf
+plt.figure(figsize=(10, 5))
+plot_acf(data['y'], lags=168)  
+plt.title('Autocorrelation Plot')
+plt.show()
---
+++ regressionMAX.py
@@ -0,0 +1,56 @@
+# 必要なライブラリのインポート
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.preprocessing import RobustScaler
+import matplotlib.pyplot as plt
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
+from sklearn.decomposition import PCA
+
+data = pd.read_csv("/Users/isogaiyuya/Downloads/温泉道場/python/DC -project/integrated_train_data.csv")
+df = pd.DataFrame(data)
+# データの準備
+X = df[['MUFL', 'LUFL', 'MULL', 'LULL']] # 特徴量
+y = df['OT']  # 目的変数
+
+# データ分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 主成分分析の実行
+pca = PCA(n_components=len(X.columns))  # 全ての主成分を取得
+pca.fit(X_train)
+
+# 各主成分の寄与率を確認
+explained_variance_ratio = pca.explained_variance_ratio_
+print(f"Explained Variance Ratio: {explained_variance_ratio}")
+
+# 定数項を追加（必要条件）
+X_with_constant = add_constant(X_train)
+
+# VIFの計算
+vif_data = pd.DataFrame()
+vif_data["Feature"] = X.columns
+vif_data["VIF"] = [variance_inflation_factor(X_with_constant.values, i+1) for i in range(X.shape[1])]
+
+# 出力
+print(vif_data)
+
+# モデルの作成と訓練
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# モデルの評価
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)  
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)            # 決定係数
+mae = mean_absolute_error(y_test, y_pred)
+
+print(f"MAE: {mae}")
+print(f"RMSE: {rmse}")
+print(f"R^2: {r2}")
+
+
